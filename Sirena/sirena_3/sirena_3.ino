240f89c91d840bf8d753cf6198df80c241c53604
SergiVinals2021 <93133749+SergiVinals2021@users.noreply.github.com>
2022-01-25 18:08:57 +0100

    Sirena

--- Sirena/sirena_3/sirena_3.ino
+++ Sirena/sirena_3/sirena_3.ino
@@ -17,7 +17,6 @@ void setup()
 {
   pinMode(xiulet, OUTPUT);   // definir el pin 9 com una sortida
 }
-
 //********** Loop *****************************************************************
 void loop()
 {
--- Sirena/sirena_5/sirena_5.ino
+++ Sirena/sirena_5/sirena_5.ino
@@ -10,26 +10,22 @@
 //********** Variables ************************************************************
 const byte xiulet = 9;        // donar nom al pin 9 de l’Arduino
 const byte pot0 = A0;         // donar nom al pin A0 de l’Arduino
-int valPot0;                 // guardar valor del potenciometre  
-
+const byte pot1 = A1;         // donar nom al pin A0 de l’Arduino
+int valPot0;                 // guardar valor del potenciometre  0
+int valPot1;                 // guardar valor del potenciometre 1
 //********** Setup ****************************************************************
 void setup()
 {
   pinMode(xiulet, OUTPUT);   // definir el pin 9 com una sortida
 }
-
 //********** Loop *****************************************************************
 void loop()
 {
-  //valPot0 = analogRead(pot0);    // llegir valor potenciòmetre  //en aquest cas no fa falta  
+  valPot0 = analogRead(pot0);    // llegir valor potenciòmetre
+  valPot1 = analogRead(pot1);    // llegir valor potenciòmetre   
 
-  tone(xiulet, 200, 1000);    // xiulet de durada valPot0
-  delay(200+500);              // esperar valPot0 del xiulet + valPot0 silenci
-   tone(xiulet, 250, 1000);    // xiulet de durada valPot0
-  delay(100+500);              // esperar valPot0 del xiulet + valPot0 silenci
-   tone(xiulet, 500, 1000);    // xiulet de durada valPot0
-  delay(50+500);              // esperar valPot0 del xiulet + valPot0 silenci
-   tone(xiulet, 300, 1000);    // xiulet de durada valPot0
-  delay(100+2000);              // esperar valPot0 del xiulet + valPot0 silenci
+  tone(xiulet, valPot0, valPot1);    // xiulet de durada valPot0
+  delay(2*valPot1);              // esperar valPot0 del xiulet + valPot0 silenci
 }
+
 //********** Funcions *************************************************************
